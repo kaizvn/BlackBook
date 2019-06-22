e72c9fa4e444b62b160b9a9b65d5c876efa6fae1
Phuoc Chau <vodiepphuocchau@gmail.com>
2019-06-22 21:27:25 +0700

    temp

--- components/ReportFormComponent.js
+++ components/ReportFormComponent.js
@@ -1,16 +1,10 @@
 import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-
-import MLink from '../src/MLink';
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -31,75 +25,61 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(3, 0, 2)
   }
 }));
-
+// const withPreviewState = withState('preview', 'setPreview', '');
+// const withSrcState = withState('src', 'setSrc', '');
 const ReportFormComponent = () => {
   const classes = useStyles();
-
   return (
-    <Container component="main" maxWidth="xs">
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Report
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="name"
-            label="Boomer Name"
-            id="name"
-            autoComplete="name"
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="address"
-            label="Address"
-            name="address"
-            autoComplete="address"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="phone"
-            label="Phone Number"
-            id="phone"
-            autoComplete="phone"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <MLink href="#" variant="body2">
-                Forgot password?
-              </MLink>
-            </Grid>
-            <Grid item>
-              <MLink href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </MLink>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+    <React.Fragment>
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Report
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="name"
+              label="Boomer Name"
+              id="name"
+              autoComplete="name"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="address"
+              label="Address"
+              name="address"
+              autoComplete="address"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="phone"
+              label="Phone Number"
+              id="phone"
+              autoComplete="phone"
+            />
+            <div>
+              <ImageUpload width={300} height={250} />
+              <img alt="Preview" />
+            </div>
+          </form>
+        </div>
+      </Container>
+    </React.Fragment>
   );
 };
+
 export default ReportFormComponent;
