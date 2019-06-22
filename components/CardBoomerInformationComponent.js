a86d6cd949b77a7510a0e01b4d593ede7738bb32
Phuoc Chau <vodiepphuocchau@gmail.com>
2019-06-22 17:33:21 +0700

    restyle boomer information

--- components/CardBoomerInformationComponent.js
+++ components/CardBoomerInformationComponent.js
@@ -33,51 +33,60 @@ const useStyles = makeStyles({
   },
   cardRegretted: {
     backgroundColor: themeColor.regretted
+  },
+  cardHeader: {
+    height: '50px'
   }
 });
 
 export const CardBoomerInformationComponent = ({ boomer }) => {
   const classes = useStyles();
   return (
-    <Grid
-      container
-      spacing={5}
-      className={classes.cardGrid}
-      alignItems="center"
-      justify="center"
-      alignContent="center"
-    >
-      <Grid item key={boomer.name} xs={12} md={6}>
-        <CardActionArea component="a" href="#">
-          <Card
-            className={clsx(
-              classes.card,
-              boomer.status === DANGER && classes.cardDanger,
-              boomer.status === WARNING && classes.cardWarning,
-              boomer.status === REGRETTED && classes.cardRegretted
-            )}
-          >
-            <div className={classes.cardDetails}>
-              <CardContent>
-                <Typography component="h2" variant="h5">
-                  {boomer.name}
-                </Typography>
-                <Typography variant="subtitle1" color="textPrimary" paragraph>
-                  {boomer.phone}
-                </Typography>
-                <Typography variant="subtitle1">{boomer.address}</Typography>
-              </CardContent>
-            </div>
-            <Hidden>
-              <CardMedia
-                className={classes.cardMedia}
-                image="../static/boat.jpg"
-                title="Image title"
-              />
-            </Hidden>
-          </Card>
-        </CardActionArea>
+    <React.Fragment>
+      <Grid
+        container
+        spacing={5}
+        className={classes.cardGrid}
+        alignItems="center"
+        justify="center"
+        alignContent="center"
+      >
+        <Grid item key={boomer.name} xs={12} md={6}>
+          <CardActionArea component="a" href="#">
+            <Card className={classes.card}>
+              <div className={classes.cardDetails}>
+                <Card
+                  item
+                  className={clsx(
+                    classes.cardHeader,
+                    boomer.status === DANGER && classes.cardDanger,
+                    boomer.status === WARNING && classes.cardWarning,
+                    boomer.status === REGRETTED && classes.cardRegretted
+                  )}
+                  xs={12}
+                  md={6}
+                />
+                <CardContent>
+                  <Typography component="h2" variant="h5">
+                    {boomer.name}
+                  </Typography>
+                  <Typography variant="subtitle1" color="textPrimary" paragraph>
+                    {boomer.phone}
+                  </Typography>
+                  <Typography variant="subtitle1">{boomer.address}</Typography>
+                </CardContent>
+              </div>
+              <Hidden>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="../static/boat.jpg"
+                  title="Image title"
+                />
+              </Hidden>
+            </Card>
+          </CardActionArea>
+        </Grid>
       </Grid>
-    </Grid>
+    </React.Fragment>
   );
 };
