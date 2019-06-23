3c7acec67103dc517d4a8bee2f36618f4e7bc333
thaind97 <thaind97.dev@gmail.com>
2019-06-23 12:57:23 +0700

    add saveme

---
+++ pages/saveme.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainPageLayout from '../layouts/MainPageLayout';
+import SaveMeComponent from '../components/SaveMeComponent';
+
+const DetailPage = () => {
+  return (
+    <MainPageLayout>
+      <SaveMeComponent />
+    </MainPageLayout>
+  );
+};
+
+export default DetailPage;
